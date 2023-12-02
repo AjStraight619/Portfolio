61638d75ddcc62642228aa4fe3bf88316c8e4668
Alex Straight <compsciaj@outlook.com>
2023-12-01 19:45:28 -0800

    Added direct email section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Contact from "@/components/Contact";
+
 import Intro from "@/components/intro";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <Contact />
     </main>
   );
 }
