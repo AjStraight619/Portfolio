8452a9171264d46dd4100d8384492563a6605fb9
Alex Straight <alex@Alexs-Laptop.local>
2024-04-09 10:41:33 -0700

    updated resume

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -36,7 +36,7 @@ export default function Intro() {
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-24 w-24 rounded-full object-cover border-[0.2rem] border-white shadow-xl"
             />
           </motion.div>
         </div>
@@ -78,7 +78,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Alex_Straight_Resume.pdf"
+          href="/Alex_Resume.pdf"
           download
         >
           Resume{" "}
